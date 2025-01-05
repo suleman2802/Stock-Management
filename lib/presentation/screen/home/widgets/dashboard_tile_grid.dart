1c364c9087f3967405c56d81c03ebd63aa005520
Suleman Ahmad <118665029+suleman2802@users.noreply.github.com>
2025-01-05 13:33:47 +0500

    tile style

--- lib/presentation/screen/home/widgets/dashboard_tile_grid.dart
+++ lib/presentation/screen/home/widgets/dashboard_tile_grid.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class DashboardTileGrid extends StatelessWidget {
   const DashboardTileGrid({
     super.key,
@@ -12,19 +11,29 @@ class DashboardTileGrid extends StatelessWidget {
   final IconData icon;
   final GestureTapCallback? onTap;
 
-
   @override
   Widget build(BuildContext context) {
     return GridTile(
       child: InkWell(
         onTap: onTap,
-        child: Card(
+        child: Container(
+          decoration: BoxDecoration(
+            border:
+                Border.all(color: Theme.of(context).primaryColor, width: 2.0),
+            borderRadius: BorderRadius.circular(10),
+          ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Icon(icon,size: 50,),
-              Text(title,style: Theme.of(context).textTheme.titleMedium,),
+              Icon(
+                icon,
+                size: 50,
+              ),
+              Text(
+                title,
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
             ],
           ),
         ),
